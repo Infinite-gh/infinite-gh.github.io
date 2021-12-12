334c553a2db297f24091c1d7c86a17608bfd1afd
KZacharski <kacperzacharski@icloud.com>
2021-12-12 10:25:41 +0100

    Started working on a new website. The div is centred.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+		backgroundImage: {
+			'logo-light': "url('/assets/logo-light-mode.png')",
+	},
+    },
+  },
+  plugins: [],
+}
